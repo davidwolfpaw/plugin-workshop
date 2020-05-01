cafb94789431469d00572709caa7f5aa81009bae
David Wolfpaw <david@davidwolfpaw.com>
2020-04-30 22:15:03 -0400

    adds main plugin file and plugin header comments

---
+++ notification-bar.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Notification Bar
+Plugin URI: https://fixupfox.com/
+Description: Display a notification for visitors on the frontend of your site!
+Version: 1.0
+Author: wolfpaw
+Author URI: https://davidwolfpaw.com/
+*/
