7265a6d38dc84358aca23f7c5ffaf2961d67a8aa
David Wolfpaw <david@davidwolfpaw.com>
2020-04-30 22:19:25 -0400

    adds submenu item for settings page in WordPress dashboard

--- notification-bar.php
+++ notification-bar.php
@@ -7,3 +7,18 @@ Version: 1.0
 Author: wolfpaw
 Author URI: https://davidwolfpaw.com/
 */
+
+/**
+ * Creates a link to the settings page under the WordPress Settings in the dashboard
+ */
+add_action( 'admin_menu', 'wnb_general_settings_page' );
+function wnb_general_settings_page() {
+	add_submenu_page(
+		'options-general.php',
+		__( 'Notification Bar', 'notification-bar' ),
+		__( 'Notification Bar', 'notification-bar' ),
+		'manage_options',
+		'notification_bar',
+		'wnb_render_settings_page'
+	);
+}
